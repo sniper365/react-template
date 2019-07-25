fe946ac3d810a02d9926a161ad01be28bed686b9
sniper365 <devtops2050@gmail.com>
2019-07-25 23:42:27 +0100

    adding express and compression need compression plugin

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,77 +1,89 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CompressionPlugin = require('compression-webpack-plugin')
 
-module.exports=()=>({
-	mode:"development",
-	devtool: "eval-source-map",
-	devServer : {
-		port:9000,
-		compress: true,
-		serveIndex: true,
-	},
-	module:{
-		rules : [
-			{
-				test:/\.(js|jsx)$/,
-				exclude : [/node_modules/,/utils/],
-				use:["babel-loader",{
-					loader:"eslint-loader",
-					options : {
-						emitWarning: true
-					}
-				}]
-			}, {
-				test: /\.(css)$/,
-				use: [ MiniCssExtractPlugin.loader, 'css-loader'],
-			}, {
-				test: /\.(png|jpg|gif|jpe?g|svg|webp)$/i,
-				use: [
-					{
-						loader: 'url-loader',
-						options: {
-							limit: 5000,
-							outputPath: './assets/images',
-						}
-					},{
-						loader: "image-webpack-loader",
-						options: {
-							mozjpeg : {
-								progressive : true,
-								quality : 65
-							},
-							optipng : {
-								enabled : false,
-							},
-							pngquant : {
-								quality : "65-90",
-								speed : 4
-							},
-							gifsicle : {
-								interlaced : false,
-							},
-							webp : {
-								quality : 75
-							},
-							svgo:{
-								removeViewBox: false
-							}
-						}
-					}
-				]
-			},{
-				test: /\.(woff|woff2|eot|ttf|otf)$/i,
-				use: [
-					{
-						loader: 'url-loader',
-						options: {
-							outputPath: './assets/fonts',
-						}
-					}
-				]
-			}
-		],
-	},
-	plugins: [
-		new MiniCssExtractPlugin(),
-	]
-});
-
+module.exports = () => ({
+  mode: 'development',
+  devtool: 'eval-source-map',
+  devServer: {
+    port: 9000,
+    compress: true,
+    serveIndex: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: [/node_modules/, /utils/],
+        use: [
+          'babel-loader',
+          {
+            loader: 'eslint-loader',
+            options: {
+              emitWarning: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(css)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|gif|jpe?g|svg|webp)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 5000,
+              outputPath: './assets/images'
+            }
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              optipng: {
+                enabled: false
+              },
+              pngquant: {
+                quality: '65-90',
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false
+              },
+              webp: {
+                quality: 75
+              },
+              svgo: {
+                removeViewBox: false
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              outputPath: './assets/fonts'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new CompressionPlugin({
+      deleteOriginalAssets: true,
+      algorithm: 'gzip',
+      minRatio: 0.8
+    })
+  ]
+})
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,113 +1,119 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const CompressionPlugin = require('compression-webpack-plugin')
 
-module.exports=()=>({
-	mode: "production",
-	output : {
-		filename : "[chunkhash].js"
-	},
-	module:{
-		rules : [
-			{
-				test:/\.(js|jsx)$/,
-				exclude : [/node_modules/,/utils/],
-				use:{
-					loader: "babel-loader"
-				}
-			},{
-				test: /\.css$/,
-				use: [ MiniCssExtractPlugin.loader, 'css-loader' ],
-			},{
-				test: /\.(png|jpg|gif|jpe?g|svg|webp)$/i,
-				use: [
-					{
-						loader: 'url-loader',
-						options: {
-							limit: 5000,
-							outputPath: 'assets/images',
-						},
-					},
-					{
-						loader: "image-webpack-loader",
-						options: {
-							mozjpeg : {
-								progressive : true,
-								quality : 65
-							},
-							optipng : {
-								enabled : false,
-							},
-							pngquant : {
-								quality : "65-90",
-								speed : 4
-							},
-							gifsicle : {
-								interlaced : false,
-							},
-							webp : {
-								quality : 75
-							},
-							svgo : {
-								removeViewBox : false
-							}
-						}
-					}
-				]
-			},{
-				test: /\.(woff|woff2|eot|ttf|otf)$/i,
-				use: [
-					{
-						loader: 'url-loader',
-						options: {
-							limit: 5000,
-							outputPath: 'assets/images',
-						}
-					}
-				]
-			}
-		],
-		
-	},
-	optimization: {
-		minimize: true,
-		mangleWasmImports: true,
-		removeEmptyChunks:true,
-		mergeDuplicateChunks: true,
-		minimizer: [
-			new TerserPlugin({
-				terserOptions: {
-					ecma: undefined,
-					warnings: false,
-					parse: {},
-					compress: {
-						drop_console:true
-					},
-					mangle: true, // Note `mangle.properties` is `false` by default.
-					module: false,
-					output: null,
-					toplevel: false,
-					nameCache: null,
-					ie8: false,
-					keep_classnames: undefined,
-					keep_fnames: false,
-					safari10: false,
-					cache: true,
-					parallel: true,
-				}
-			}),
-			new OptimizeCSSAssetsPlugin({
-				filename: '[name].css',
-				chunkFilename: '[id].css',
-			})
-		],
-	},
-	plugins: [
-		new CleanWebpackPlugin(),
-		new MiniCssExtractPlugin({
-			filename:  '[name].[hash].css',
-			chunkFilename: '[id].[hash].css'
-		})
-	]
-});
+module.exports = () => ({
+  mode: 'production',
+  output: {
+    filename: '[chunkhash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: [/node_modules/, /utils/],
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|gif|jpe?g|svg|webp)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 5000,
+              outputPath: 'assets/images'
+            }
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              optipng: {
+                enabled: false
+              },
+              pngquant: {
+                quality: '65-90',
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false
+              },
+              webp: {
+                quality: 75
+              },
+              svgo: {
+                removeViewBox: false
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 5000,
+              outputPath: 'assets/images'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  optimization: {
+    minimize: true,
+    mangleWasmImports: true,
+    removeEmptyChunks: true,
+    mergeDuplicateChunks: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          ecma: undefined,
+          warnings: false,
+          parse: {},
+          compress: {
+            drop_console: true
+          },
+          mangle: true, // Note `mangle.properties` is `false` by default.
+          module: false,
+          output: null,
+          toplevel: false,
+          nameCache: null,
+          ie8: false,
+          keep_classnames: undefined,
+          keep_fnames: false,
+          safari10: false,
+          cache: true,
+          parallel: true
+        }
+      }),
+      new OptimizeCSSAssetsPlugin({
+        filename: '[name].css',
+        chunkFilename: '[id].css'
+      })
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash].css',
+      chunkFilename: '[id].[hash].css'
+    }),
+    new CompressionPlugin({
+      deleteOriginalAssets: true
+    })
+  ]
+})
