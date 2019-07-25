33378bdd86e31cb323e720a10587e3314802beaa
sniper365 <devtops2050@gmail.com>
2019-07-25 22:35:08 +0100

    finxg webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,47 @@
-const path = require('path'),
-	webpack = require('webpack'),
-	merge = require("webpack-merge"),
-	HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path')
 
-const modeConfig = env => require(`./webpack/webpack.${env}.js`)(env);
-module.exports=({mode,presets}={mode: "dev", presets:[]})=>{
-return merge({
-	mode,
-	entry:"./index.js",
-	output:{
-		filename: "./main.js",
-	},
-	resolve : {
-		alias:{
-			"Components": path.resolve(__dirname,"src/Components"),
-			"css": path.resolve(__dirname,"./src/css"),
-			"mocks": path.resolve(__dirname,"./src/__mocks__"),
-			"utils": path.resolve(__dirname,"./utils"),
-			"assets" : path.resolve(__dirname,"./assets"),
-			"src": path.resolve(__dirname,"./src"),
-		},
-		modules: [path.resolve(__dirname,"./src"),"node_modules"],
-	},
-	plugins: [
-		new webpack.ProgressPlugin(),
-		new HtmlWebpackPlugin({
-			template: path.resolve('./webpack/template.ejs') ,
-			templateParameters:{
-				title: "React BoilerPlate v3"  // name of the app
-			},
-			inject:true,
-			minify: {
-				removeComments: true,
-				collapseWhitespace: true
-			}
-		})
-	]
-},modeConfig(mode));
-};
+const webpack = require('webpack')
+
+const merge = require('webpack-merge')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const modeConfig = env => require(`./webpack/webpack.${env}.js`)(env)
+
+module.exports = ({ mode, presets } = { mode: 'dev', presets: [] }) => {
+  return merge(
+    {
+      mode,
+      entry: './index.js',
+      output: {
+        filename: './main.js'
+      },
+      resolve: {
+        alias: {
+          Components: path.resolve(__dirname, 'src/Components'),
+          css: path.resolve(__dirname, './src/css'),
+          mocks: path.resolve(__dirname, './src/__mocks__'),
+          utils: path.resolve(__dirname, './utils'),
+          assets: path.resolve(__dirname, './assets'),
+          src: path.resolve(__dirname, './src')
+        },
+        modules: [path.resolve(__dirname, './src'), 'node_modules']
+      },
+      plugins: [
+        new webpack.ProgressPlugin(),
+        new HtmlWebpackPlugin({
+          template: path.resolve('./webpack/template.ejs'),
+          templateParameters: {
+            title: 'React BoilerPlate v3' // name of the app
+          },
+          inject: true,
+          minify: {
+            removeComments: true,
+            collapseWhitespace: true
+          }
+        })
+      ]
+    },
+    modeConfig(mode)
+  )
+}
