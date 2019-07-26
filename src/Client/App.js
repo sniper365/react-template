dbce8aa72c4fcc48ef6a6843f080cbfd3ab4414a
sniper365 <devtops2050@gmail.com>
2019-07-26 18:24:47 +0100

    rearranging client and servers changes so webpack can bundle accordingly

--- src/Client/App.js
+++ src/Client/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactSVGComponent from "Components/ReactSVGComponent/ReactSVGComponent";
-import "src/css/main.css";
-const App = ()=><div>
-	<div className="container">
-		<h1>This is a heading</h1>
-		<p>This is a paragraph</p>
-		<ReactSVGComponent />
-	</div>
-</div>;
+import "css/main.css";
+const App = () => (
+  <div>
+    <div className="container">
+      <h1>This is a heading</h1>
+      <p>This is a test</p>
+      <ReactSVGComponent />
+    </div>
+  </div>
+);
 export default App;
--- src/server/server.js
+++ src/server/server.js
@@ -1,15 +1,15 @@
-const express = require('express')
-const expressStaticGzip = require('express-static-gzip')
-const app = express()
+import express from "express";
+import expressStaticGzip from "express-static-gzip";
+const app = express();
 var options = {
   enableBrotli: true,
-  orderPreference: ['br', 'gz'],
-  setHeaders: function (res, path) {
-    res.setHeader('Cache-Control', 'public, max-age=31536000')
-  }
-}
-
-app.use(expressStaticGzip('./dist', options))
-app.listen('9001', () => {
-  console.log('App started at port number 9000')
-})
+  orderPreference: ["br", "gz"],
+  setHeaders: function(res, path) {
+    res.setHeader("Cache-Control", "public, max-age=31536000");
+  },
+};
+console.log("test", app);
+app.use(expressStaticGzip("./dist", options));
+app.listen("9001", () => {
+  console.log("App started at port number 9000");
+});
