628163d8462e50f9c1eb6bbf1d510bb91e2fb44b
sniper365 <devtops2050@gmail.com>
2019-07-26 21:42:09 +0100

    adding some changes

--- src/server/server.js
+++ src/server/server.js
@@ -2,14 +2,14 @@ import express from "express";
 import expressStaticGzip from "express-static-gzip";
 const app = express();
 var options = {
-  enableBrotli: true,
-  orderPreference: ["br", "gz"],
-  setHeaders: function(res, path) {
-    res.setHeader("Cache-Control", "public, max-age=31536000");
-  },
+	enableBrotli: true,
+	orderPreference: ["br", "gz"],
+	setHeaders: function(res, path) {
+		res.setHeader("Cache-Control", "public, max-age=31536000");
+	},
 };
 console.log("test", app);
-app.use(expressStaticGzip("./dist", options));
+app.use(expressStaticGzip("./ClientBundle", options));
 app.listen("9001", () => {
-  console.log("App started at port number 9000");
+	console.log("App started at port number 9000");
 });
